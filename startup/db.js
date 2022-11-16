6f6e03c990a6dd370b662ff1c2d0296eff2201a3
Abhinav7272 <gaurabhinav54@gmail.com>
2022-11-17 01:27:17 +0530

    load balancer

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const config = require('config');
 
 module.exports = function() {
-  const db = config.get('db');
-  mongoose.connect(db)
-    .then(() => winston.info(`Connected to ${db}...`));
+  const db1 = config.get('db1');
+  const db2 = config.get('db2');
+
+  mongoose.connect(db1)
+    .then(() => winston.info(`Connected to ${db1} and ${db2}...`));
 }
